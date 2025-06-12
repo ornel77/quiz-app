373410965f5d914fdeff6e82258bfa9431dbfd4a
ornel77 <ornella.tomboza@gmail.com>
2025-06-12 21:25:12 +0200

    react router and start page ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-
+import { Route, Routes } from "react-router-dom";
+import StartPage from "./pages/StartPage";
+import QuestionsPage from "./pages/QuestionsPage";
+import ScorePage from "./pages/ScorePage";
 
 function App() {
-
   return (
-    <div className="bg-blue-500 p-2300 text-[144px] text-white uppercase font-rubik">
-      hello
+    <div className="background bg-blue-900  text-white font-rubik h-screen p-7">
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/questions" element={<QuestionsPage />} />
+        <Route path="/score" element={<ScorePage />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,6 +1,54 @@
+import { Link } from "react-router-dom";
+
 const StartPage = () => {
   return (
-    <div>StartPage</div>
-  )
-}
-export default StartPage
+    <>
+      <div className="theme flex justify-end py-6">
+        <h2>THEME</h2>
+      </div>
+      <section className="welcome mb-10 pt-8">
+        <h1 className="text-[40px] leading-12">
+          Welcome to the <span className="font-bold">Frontend Quiz!</span>
+        </h1>
+        <p className="text-blue-300 text-[14px] mt-3 italic ">
+          Pick a subject to get started.
+        </p>
+      </section>
+      <section className="themes">
+        <Link to={"/questions"} className="flex items-center p-4 bg-blue-850 rounded-2xl gap-4 mb-4">
+          <img
+            src="/public/assets/images/icon-html.svg"
+            alt="html"
+            className="size-10 bg-orange-50 p-1.5 rounded-[8px]"
+          />
+          <p className="text-[18px]">HTML</p>
+        </Link>
+        <Link to={"/questions"} className="flex items-center p-4 bg-blue-850 rounded-2xl gap-4 mb-4">
+          <img
+            src="/public/assets/images/icon-css.svg"
+            alt="html"
+            className="size-10 bg-green-100 p-1.5 rounded-[8px]"
+          />
+          <p className="text-[18px]">CSS</p>
+        </Link>
+        <Link to={"/questions"} className="flex items-center p-4 bg-blue-850 rounded-2xl gap-4 mb-4">
+          <img
+            src="/public/assets/images/icon-js.svg"
+            alt="html"
+            className="size-10 bg-blue-50 p-1.5 rounded-[8px]"
+          />
+          <p className="text-[18px]">Javascript</p>
+        </Link>
+        <Link to={"/questions"} className="flex items-center p-4 bg-blue-850 rounded-2xl gap-4 mb-4">
+          <img
+            src="/public/assets/images/icon-accessibility.svg"
+            alt="html"
+            className="size-10 bg-purple-100 p-1.5 rounded-[8px]"
+          />
+          <p className="text-[18px]">Accessibility</p>
+        </Link>
+      </section>
+    </>
+  );
+};
+export default StartPage;
