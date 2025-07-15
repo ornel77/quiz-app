a8d071f5b158a46d7c4de99e4bf9281aec0d29cf
ornel77 <ornella.tomboza@gmail.com>
2025-07-15 14:01:01 +0200

    still debugging the result of a question

--- src/components/Quizz/QuizzContainer.jsx
+++ src/components/Quizz/QuizzContainer.jsx
@@ -11,6 +11,7 @@ const QuizzContainer = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [selectedOption, setSelectedOption] = useState("");
   const [isCorrect, setIsCorrect] = useState();
+  const [isSubmit, setIsSubmit] = useState();
   const [score, setScore] = useState(0);
 
   const navigate = useNavigate();
@@ -42,12 +43,13 @@ const QuizzContainer = () => {
   const progress = ((currentQuestionIndex + 1) * 100) / questions.length;
   const handleSubmit = (e) => {
     e.preventDefault();
+    setIsSubmit(true);
     const isAnswerCorrect = selectedOption === currentQuestion.answer;
     setIsCorrect(isAnswerCorrect);
     setTimeout(() => {
       if (isAnswerCorrect) {
         setScore((prev) => prev + 1);
-        console.log(isCorrect)
+        console.log(isCorrect);
       }
       setSelectedOption("");
 
@@ -58,6 +60,7 @@ const QuizzContainer = () => {
       }
 
       setIsCorrect(null);
+      setIsSubmit(false);
     }, 2000);
   };
 
@@ -84,43 +87,58 @@ const QuizzContainer = () => {
         {/* Choix multiple */}
 
         <form className="lg:flex-1 text-[18px] flex flex-col">
-          {currentQuestion?.options?.map((option, i) => (
-            <label htmlFor={option} className="flex items-center" key={i}>
-              <input
-                type="radio"
-                name="options"
-                id={option}
-                className="peer hidden"
-                value={option}
-                checked={selectedOption === option}
-                onChange={(e) => setSelectedOption(e.target.value)}
-              />
-              <div className="question w-full bg-blue-850 mb-6 rounded-[12px] py-4 flex ring-2 ring-transparent peer-checked:ring-purple-600 peer-hover:ring-purple-600 items-center justify-between px-3">
-                <div className="flex items-center gap-5">
-                  <span className="letter-question text-gray-500 text-[28px] bg-grey-50 flex justify-center items-center rounded-xl w-[30px] h-[30px] p-6 ">
-                    {" "}
-                    {questionNumber[i]}{" "}
-                  </span>{" "}
-                  <span className="text-[18px]">{option}</span>
+          {currentQuestion?.options?.map((option, i) => {
+            const isRightAnswer = option === currentQuestion.answer;
+            const isSelected = option === selectedOption;
+            return (
+              <label htmlFor={option} className="flex items-center" key={i}>
+                <input
+                  type="radio"
+                  name="options"
+                  id={option}
+                  className="peer hidden"
+                  value={option}
+                  checked={selectedOption === option}
+                  onChange={(e) => setSelectedOption(e.target.value)}
+                />
+                <div
+                  className={`question w-full bg-blue-850 mb-6 rounded-[12px] py-4 flex ring-2 ring-transparent  items-center justify-between px-3 
+                    ${isSelected && 'peer-checked:ring-purple-600'}
+                    ${isSubmit && isRightAnswer && "green-border"}
+                    ${isSubmit && !isRightAnswer && isSelected ? "red-border" : ""}
+                    `}
+                >
+                  <div className="flex items-center gap-5">
+                    <span className={`letter-question text-gray-500 text-[28px] bg-grey-50 flex justify-center items-center rounded-xl w-[30px] h-[30px] p-6 
+                    ${isSubmit && isRightAnswer ? "green-color" : ""}
+                    ${isSubmit && !isRightAnswer && isSelected ? "red-color" : ""}
+
+                      `}>
+                      {" "}
+                      {questionNumber[i]}{" "}
+                    </span>{" "}
+                    <span className="text-[18px]">{option}</span>
+                  </div>
+                  <div className="flex flex-shrink-0 w-10 h-10 items-center justify-center ">
+                    {isSubmit && isRightAnswer && (
+                      <img
+                        src="/assets/images/icon-correct.svg"
+                        alt=""
+                        className="w-8 h-8"
+                      />
+                    )}
+                    {isSubmit && isSelected && !isRightAnswer && (
+                      <img
+                        src="/assets/images/icon-incorrect.svg"
+                        alt=""
+                        className="w-8 h-8"
+                      />
+                    )}
+                  </div>
                 </div>
-                <div className="flex flex-shrink-0 w-10 h-10 items-center justify-center ">
-                  {isCorrect ? (
-                    <img
-                      src="/assets/images/icon-correct.svg"
-                      alt=""
-                      className="w-8 h-8"
-                    />
-                  ) : (
-                    <img
-                      src="/assets/images/icon-incorrect.svg"
-                      alt=""
-                      className="w-8 h-8"
-                    />
-                  )}
-                </div>
-              </div>
-            </label>
-          ))}
+              </label>
+            );
+          })}
           <button
             onClick={handleSubmit}
             className="text-center inline-block w-full hover:bg-purple-600t bg-purple-600 md:rounded-3xl rounded-[12px] py-5 md:text-2xl text-[18px] cursor-pointer lg:transition-colors"
